620bdc505e67cb07d50def9d0826f8f49e9f32cf
Sanjay Ramjee <sanjay@Sanjays-MacBook-Pro.local>
2024-08-04 18:31:04 -0400

    Resolved issue with using a t test for second hypothesis and used a proportions test instead, since the data is dealing with overall percentages of the deceased. Also fixed some comments and included a conclusion.

--- script.R
+++ script.R
@@ -43,17 +43,36 @@ t.test(alive$age, dead$age, alternative = "two.sided", conf.level = 0.95)
 #we are 95% sure that the difference in ages can range from around ~17 years younger 
 # to ~24 years younger.  
 
+#therefore, we can say that there exists an age gap between patients deceased from COVID 19,
+#disproving the null hypotheses that there is no age gap. 
+
 
 #analyzing whether gender has an effect on death from COVID 19
 
 men = subset(data, gender == "male")
 women = subset(data, gender == "female")
 
+total_men = nrow(men)
+total_women = nrow(women)
 mean(men$death_dummy, na.rm = TRUE) #men have death rate of 8.5%
 mean(women$death_dummy, na.rm = TRUE) #women have death rate of 3.7%
 #running a 99% confidence interval
 #null hypothesis: gender has no effect on COVID 19 deaths
-t.test(men$death_dummy, women$death_dummy, alternative = "two.sided", conf.level = 0.99)
-#since our p value = 0.002, and is < 0.05, our results are statistically significant
-#our 99% confidence interval indicates that men are anywhere from 0.008 to 0.088% more likely to die due to COVID 19 compared to women
-#therefore, we reject the null hypothesis
+
+men_death = sum(men$death_dummy, na.rm = TRUE)
+women_death = sum(women$death_dummy, na.rm = TRUE)
+prop.test(c(men_death,women_death), c(total_men,total_women), alternative = "two.sided", conf.level = 0.99)
+#since our p value = 0.0057, and is < 0.05, our results are statistically significant
+#our 99% confidence interval indicates that in 99% of confidence intervals, the true difference in proportions of death between men and women lies
+#anywhere from 0.57% to 9% between men and women.
+#therefore, we reject the null hypothesis, meaning that we reject the claim that gender does not have an effect, since we can clearly 
+#see a difference in death rate.
+
+#Based off these two statistical analyses, we can reject the claim that age and gender don't play a role in those with COVID 19
+#who are deceased. Our use of confidence intervals using T distributions and sample proportions helps us understand whether 
+#these differences are statistically significant, i.e whether they allow us to reject the null hypotheses in a two sided test
+#to prove that they are NOT equal. 
+
+#In conclusion, this project proves that there exists some age gap and gender difference that leans one way or another when 
+#analyzing those deceased from COVID-19.
+
